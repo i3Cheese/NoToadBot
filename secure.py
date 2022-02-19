82382b080758161a8702798cd473cd2d6376c767
i3cheese <i3cheese@yandex.ru>
2022-02-19 12:29:30 +0700

    execute callback in not trusted chats

--- secure.py
+++ secure.py
@@ -20,7 +20,8 @@ def secure_callback(func: Callable[[Update, CallbackContext], None]) -> Callable
             logger.info("%d trying to use secure command", chat.id) 
             assert(update.effective_message is not None)
             update.effective_message.reply_text("У вас нет доступа к этой команде", parse_mode='MarkdownV2')
-        return func(update, callback)
+        else:
+            return func(update, callback)
 
     return _func
 
